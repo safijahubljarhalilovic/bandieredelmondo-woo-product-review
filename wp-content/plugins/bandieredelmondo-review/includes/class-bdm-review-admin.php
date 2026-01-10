0a160ee6ccd165c73f713d91c2480380901d7d6c
Safija Hubljar <safijahubljarhalilovic@outlook.com>
2026-01-10 14:20:48 -0500

    small fix

--- wp-content/plugins/bandieredelmondo-review/includes/class-bdm-review-admin.php
+++ wp-content/plugins/bandieredelmondo-review/includes/class-bdm-review-admin.php
@@ -49,9 +49,9 @@ class BDM_Review_Admin {
       $c = (int) get_post_meta($post_id, '_bdm_certified', true);
       // traffic-light indicator
       if ($c) {
-        echo '<span style="display:inline-block;width:12px;height:12px;border-radius:50%;background:#0a7;" title="Certificato"></span>';
+        echo '<span style="display:inline-block;width:12px;height:12px;border-radius:50%;background:#0a7;" title="Certificato">Certificato</span>';
       } else {
-        echo '<span style="display:inline-block;width:12px;height:12px;border-radius:50%;background:#f2b400;" title="Spontaneo"></span>';
+        echo '<span style="display:inline-block;width:12px;height:12px;border-radius:50%;background:#f2b400;" title="Spontaneo">Spontaneo</span>';
       }
     }
     if ($col === 'bdm_status') {
